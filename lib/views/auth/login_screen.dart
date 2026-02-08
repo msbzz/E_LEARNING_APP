9a0c405e864a7a4d1fad4802c47b075ad906325b
Marco Barozzi <mbzstudent@gmail.com>
2026-02-08 20:37:46 -0300

    login screem part1

--- lib/views/auth/login_screen.dart
+++ lib/views/auth/login_screen.dart
@@ -5,6 +5,42 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('Loging Sceen')));
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: size.height * 0.3,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Theme.of(context).primaryColor,
+                    Theme.of(context).primaryColor.withValues(alpha: 0.8),
+                  ],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                ),
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(100),
+                ),
+              ),
+              child: const Stack(
+                children: [
+                  Center(
+                    child: Column(
+                      children: [
+                        Icon(Icons.school, size: 50, color: Colors.white),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
