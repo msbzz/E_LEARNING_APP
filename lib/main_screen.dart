41fe888a5c52b6758fd1d68218dd957ff5ba5add
Marco Barozzi <mbzstudent@gmail.com>
2026-02-09 19:13:27 -0300

    Sleek Bottom Navigation Bar UI

---
+++ lib/main_screen.dart
@@ -0,0 +1,40 @@
+import 'package:e_learning_app/bloc/navigation/navigation_bloc.dart';
+import 'package:e_learning_app/bloc/navigation/navigation_event.dart';
+import 'package:e_learning_app/bloc/navigation/navigation_state.dart';
+import 'package:e_learning_app/core/theme/app_colors.dart';
+import 'package:e_learning_app/views/course/course_list/course_list_screen.dart';
+import 'package:e_learning_app/views/home/home_screen.dart';
+import 'package:e_learning_app/views/profile/profile_screen.dart';
+import 'package:e_learning_app/views/quiz/quiz_list/widget/quiz_list_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class MainScreen extends StatelessWidget {
+  final int? initialIndex;
+
+  const MainScreen({super.key, this.initialIndex});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) =>
+          NavigationBloc()..add(NavigateToTab(initialIndex ?? 0)),
+      child: BlocBuilder<NavigationBloc, NavigationState>(
+        builder: (context, state) {
+          return Scaffold(
+            backgroundColor: AppColors.lightBackground,
+            body: IndexedStack(
+              index: state.currentIndex,
+              children: [
+                HomeScreen(),
+                CourseListScreen(),
+                QuizListScreen(),
+                ProfileScreen(),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
