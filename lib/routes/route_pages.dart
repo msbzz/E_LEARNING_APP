720cbf9d99d3d0e1eed5587696481dfff1bf6a3d
Marco Barozzi <mbzstudent@gmail.com>
2026-02-09 14:01:16 -0300

    register screem part6 / refactor

--- lib/routes/route_pages.dart
+++ lib/routes/route_pages.dart
@@ -5,6 +5,7 @@ import 'package:e_learning_app/views/auth/register_screen.dart';
 import 'package:e_learning_app/views/home/home_screen.dart';
 import 'package:e_learning_app/views/onboarding/onboarding_screen.dart';
 import 'package:e_learning_app/views/splash/splash_screen.dart';
+import 'package:e_learning_app/views/teacher/teacher_home_screen.dart';
 import 'package:get/get.dart';
 
 class AppPages {
@@ -13,6 +14,7 @@ class AppPages {
     GetPage(name: AppRoutes.onboarding, page: () => const OnboardingScreen()),
     GetPage(name: AppRoutes.login, page: () => const LoginScreen()),
     GetPage(name: AppRoutes.register, page: () => const RegisterScreen()),
+    GetPage(name: AppRoutes.teacherHome, page: () => const TeacherHomeScreen()),
     GetPage(
       name: AppRoutes.forgotPassword,
       page: () => const ForgotPasswordScreen(),
