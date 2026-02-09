816875cfd6dba5a82c628238fb8c31a5d2b2b204
Marco Barozzi <mbzstudent@gmail.com>
2026-02-09 17:14:13 -0300

    Forgot sceen part3

--- lib/views/auth/forgot_password_screen.dart
+++ lib/views/auth/forgot_password_screen.dart
@@ -1,3 +1,6 @@
+import 'package:e_learning_app/core/utils/validators.dart';
+import 'package:e_learning_app/views/widgets/common/custom_button.dart';
+import 'package:e_learning_app/views/widgets/common/custom_textfield.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
@@ -19,6 +22,39 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
     super.dispose();
   }
 
+  void _handleResetPassword() {
+    Get.dialog(
+      AlertDialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadiusGeometry.circular(20),
+        ),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(Icons.mark_email_read_outlined, size: 60, color: Colors.green),
+
+            SizedBox(height: 20),
+
+            Text(
+              'Check your email',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+
+            SizedBox(height: 10),
+
+            Text(
+              'We have sent password recovery instructions to your email',
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.grey),
+            ),
+
+            SizedBox(height: 20),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,7 +69,9 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
       ),
       body: Padding(
         padding: EdgeInsets.all(20),
+
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
               'Forgot Password',
@@ -43,6 +81,28 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
             Text(
               'Enter your meil address to reset your password',
               style: const TextStyle(fontSize: 16, color: Colors.grey),
+            ),
+
+            const SizedBox(height: 10),
+
+            //email field
+            Form(
+              key: _formKey,
+              child: CustomTextfield(
+                label: 'Email',
+                prefixIcon: Icons.email_outlined,
+                contoller: _emailController,
+                keyboardType: TextInputType.emailAddress,
+                validator: FormValidator.validateEmail,
+              ),
+            ),
+
+            const SizedBox(height: 30),
+
+            //reset buttom
+            CustomButton(
+              text: 'Reset Password',
+              onPressed: _handleResetPassword,
             ),
           ],
         ),
